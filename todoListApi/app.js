945a7d38748cf32849f166e28fdff5e775edef3d
Syed Haider <shayder426@gmail.com>
2017-11-13 15:05:14 -0800

    New Server Files

--- todoListApi/app.js
+++
@@ -1,4 +0,0 @@
-// app.js
-var AuthController = require('./auth/AuthController');
-app.use('/api/auth', AuthController);
-module.exports = app;
--- todoListApi/server.js
+++ todoListApi/server.js
@@ -1,6 +1,41 @@
-var app = require('./app');
-var port = process.env.PORT || 3000;
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'), //created model loading here
+  User = require('./api/models/userModel'),
+  jsonwebtoken = require("jsonwebtoken"),
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb'); 
 
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req,res,next) {
+	if(req.headers && req.headers.authorization && req.headers.authorization.split('')[0] === 'JWT')
+	{
+		jsonwebtoken.verify(req.headers.authorization.split('')[1],'RESTFULAPIs',function(err,decode){
+			if(err)re.user = undefined;
+			req.user = decode;
+			next();
+		});
+	}
+	else
+	{
+		req.user = undefined;
+		next();
+	}
 });
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
